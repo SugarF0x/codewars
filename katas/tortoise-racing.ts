15aa98e49b685e96659d32e7b8bcc45b138036ba
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-02 08:26:27 +0300

    add a number of test cases

--- katas/tortoise-racing.ts
+++ katas/tortoise-racing.ts
@@ -7,7 +7,12 @@ function race(speedA: number, speedB: number, headstart: number): [number, numbe
 }
 
 describe('tortoise-racing', () => {
-  it.each<[[number, number, number], [number, number, number]]>([])('should return catchup time %#', (input, output) => {
+  it.each<[[number, number, number], [number, number, number]]>([
+    [[720, 850, 70], [0, 32, 18]],
+    [[80, 91, 37], [3, 21, 49]],
+    [[80, 100, 40], [2, 0, 0]],
+    [[720, 850, 37], [0, 17, 4]],
+  ])('should return catchup time %#', (input, output) => {
     expect(race(...input)).toMatchObject(output)
   })
 })
