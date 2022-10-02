b1fe58061506b7dbb86252b12442230ee5798264
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-02 19:34:00 +0300

    add big number addition solution
    its a funky one - they wanted me to use BigInt lib but i aint doing that

--- katas/add-big-numbers.ts
+++ katas/add-big-numbers.ts
@@ -3,7 +3,32 @@
 import { describe, it, expect } from "vitest"
 
 function add(a: string, b: string): string {
-  return a + b
+  const arrA = a.split('').reverse().map(Number)
+  const arrB = b.split('').reverse().map(Number)
+  const [longer, smaller] = [arrA, arrB].sort((a,b) => b.length - a.length)
+
+  const result: number[] = []
+  let hasOverflow = false
+
+  longer.forEach((digit, index) => {
+    let newDigit = digit + (smaller[index] ?? 0)
+
+    if (hasOverflow) {
+      newDigit++
+      hasOverflow = false
+    }
+
+    if (newDigit >= 10) {
+      newDigit -= 10
+      hasOverflow = true
+    }
+
+    result.push(newDigit)
+  })
+
+  if (hasOverflow) result.push(1)
+
+  return result.reverse().join('')
 }
 
 describe('add-big-numbers', () => {
@@ -16,6 +41,6 @@ describe('add-big-numbers', () => {
     [["101", "100"], "201"],
     [['63829983432984289347293874', '90938498237058927340892374089'], "91002328220491911630239667963"],
   ])('should add properly %#', (input, output) => {
-    expect(...input).toEqual(output)
+    expect(add(...input)).toEqual(output)
   })
 })
