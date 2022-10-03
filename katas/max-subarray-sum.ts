53c026d6eaa75d710d985ca54bf9568e1c59727a
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-03 05:15:39 +0300

    solve maxSequence

--- katas/max-subarray-sum.ts
+++ katas/max-subarray-sum.ts
@@ -1,7 +1,27 @@
 import { describe, expect, it } from 'vitest'
 
-function maxSequence(array: number[]): number {
-  return 0
+function maxSequence(digits: number[]): number {
+  let stack = []
+  let stackSum = 0
+
+  let maxStack = []
+  let maxStackSum = 0
+
+  for (let i = 0; i < digits.length; i++) {
+    stack = []
+    stackSum = 0
+
+    for (const digit of digits.slice(i)) {
+      stack.push(digit)
+      stackSum += digit
+      if (maxStackSum < stackSum) {
+        maxStack = [...stack]
+        maxStackSum = stackSum
+      }
+    }
+  }
+
+  return maxStackSum
 }
 
 describe('max-subarray-sum', () => {
