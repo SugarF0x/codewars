271ab8780f34336d2fd74b4d34c9918380617b9b
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-10 13:32:17 +0300

    add digit to word dictionary

--- katas/in-progress/numberToEnglish.ts
+++ katas/in-progress/numberToEnglish.ts
@@ -1,5 +1,38 @@
 import { describe, it, expect } from "vitest"
 
+const digitToWordMap: Record<number, string> = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+  100: 'hundred',
+  1000: 'thousand'
+}
+
 export function numberToEnglish(x: number): string {
   return "zero"; //your code here
 }
@@ -18,7 +51,7 @@ describe("Sample Test Cases", function(){
     [6800, 'six thousand eight hundred'],
     [14111, 'fourteen thousand one hundred eleven'],
     [20005, 'twenty thousand five'],
-    [99999, 'ninety nine thousand nine hundred ninety nin'],
+    [99999, 'ninety nine thousand nine hundred ninety nine'],
   ])("Should return a string", (input, output) => {
     expect(numberToEnglish(input)).toEqual(output);
   });
