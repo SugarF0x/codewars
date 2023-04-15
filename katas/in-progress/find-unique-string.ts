ed33d7a0b2b27e3b0db6af8b5c035eb0cd95e783
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-15 13:38:29 +0300

    add local passing solution

--- katas/in-progress/find-unique-string.ts
+++ katas/in-progress/find-unique-string.ts
@@ -1,8 +1,19 @@
 import { describe, expect, it } from "vitest"
 
 export function findUniq(arr: string[]): string {
-  // Do the magic
-  return arr[0];
+  const set = new Set<string>()
+  let uniq = ''
+
+  for (const item of arr) {
+    const characters = item.toLowerCase().replaceAll(' ', '').split('')
+
+    let isUniq = characters.some(char => !set.has(char))
+    for (const character of characters) set.add(character)
+
+    if (isUniq) uniq = item
+  }
+
+  return uniq
 }
 
 describe('find-unique-string', () => {
@@ -12,7 +23,7 @@ describe('find-unique-string', () => {
     [['silvia', 'vasili', 'victor'], 'victor'],
     [['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter'], 'Harry Potter'],
     [['    ', 'a', ' '], 'a'],
-  ])('should ', (input, output) => {
+  ])('%#', (input, output) => {
     expect(findUniq(input)).toEqual(output)
   })
 })
