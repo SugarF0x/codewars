7c61477f6b29eabbdf8d9e119a5ef8c6474d6e20
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-16 15:26:13 +0300

    finish kata

--- katas/in-progress/find-unique-string.ts
+++ katas/in-progress/find-unique-string.ts
@@ -1,21 +1,21 @@
 import { describe, expect, it } from "vitest"
 
 export function findUniq(arr: string[]): string {
-  const signatures = new Set<string>()
+  const signatureItemsMap = new Map<string, string[]>()
 
-  for (const item of arr) {
+  for (const itemKey in arr) {
+    const itemIndex = Number(itemKey)
+    const item = arr[itemIndex]
     const itemSignature = [...new Set(item.toLowerCase().replaceAll(' ', '').split('').sort())].join('')
 
-    if (!signatures.size) {
-      signatures.add(itemSignature)
-      continue
-    }
+    const currentSignatureItems = signatureItemsMap.get(itemSignature)
+    if (!currentSignatureItems) signatureItemsMap.set(itemSignature, [item])
+    else currentSignatureItems.push(item)
 
-    if (signatures.has(itemSignature)) continue
-    return item
+    if (signatureItemsMap.size === 2 && itemIndex > 1) return [...signatureItemsMap.entries()].sort((a, b) => a[1].length - b[1].length)[0][1][0]
   }
 
-  return arr[0]
+  throw new Error('No uniq found')
 }
 
 describe('find-unique-string', () => {
@@ -25,6 +25,8 @@ describe('find-unique-string', () => {
     [['silvia', 'vasili', 'victor'], 'victor'],
     [['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter'], 'Harry Potter'],
     [['    ', 'a', ' '], 'a'],
+    [[ 'o', 'b', 'b', 'b' ], 'o'],
+    [Array(10000).fill('Gollum').concat('shit'), 'shit']
   ])('%#', (input, output) => {
     expect(findUniq(input)).toEqual(output)
   })
